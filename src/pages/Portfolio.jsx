731905415a170fbbef3075dc81d30bea9884edfd
Samuel Russek <samuel_bstone_13@hotmail.com>
2023-05-24 17:55:41 -0600

    dite to docs

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -15,14 +15,14 @@ img = {"/src/pages/images/NoteTakerImg.png"}
 title = {"Weather App"}
 deployLink = {"https://samrf13.github.io/06-Weather-app/"}
 githubRepo  = {"https://github.com/SamRF13/06-Weather-app/"}
-img = {"/src/pages/images/Calendar2Logo.png"}
+img = {"/images/Calendar2Logo.png"}
 /> 
 
 <Project
 title = {"Calendar App"}
 deployLink = {"https://samrf13.github.io/05-Calendar/"}
 githubRepo  = {"https://github.com/SamRF13/05-Calendar"}
-img = {"/src/pages/images/CalendarLogo.png"}
+img = {"/images/CalendarLogo.png"}
 /> 
 
     </div>
