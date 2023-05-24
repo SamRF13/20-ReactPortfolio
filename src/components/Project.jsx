35ddfa79955b08614576aceba4b699dc762427de
Samuel Russek <samuel_bstone_13@hotmail.com>
2023-05-24 17:37:48 -0600

    asses to root

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,7 +8,7 @@ const Project = ({title, deployLink, githubRepo, img} ) => {
 <h1>{title}</h1>
 
 <a target="_blank" href={deployLink}><img src={img} alt="img" className='deployImg'/></a>
-<a target="_blank" href={githubRepo}><img src="/docs/assets/GithubLogo.png" alt="GitLogo" className='gitLogo'/></a>
+<a target="_blank" href={githubRepo}><img src="/assets/GithubLogo.png" alt="GitLogo" className='gitLogo'/></a>
     </div>
   
   
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,21 +8,21 @@ const Portfolio = () => {
 title = {"Note Taker"}
 deployLink = {"https://calendar-11.herokuapp.com/"}
 githubRepo  = {"https://github.com/SamRF13/11-Note-Creator"}
-img = {"/docs/assets/NoteTakerImg.png"}
+img = {"/assets/NoteTakerImg.png"}
 /> 
 
 <Project
 title = {"Weather App"}
 deployLink = {"https://samrf13.github.io/06-Weather-app/"}
 githubRepo  = {"https://github.com/SamRF13/06-Weather-app/"}
-img = {"assets/Calendar2Logo.png"}
+img = {"/assets/Calendar2Logo.png"}
 /> 
 
 <Project
 title = {"Calendar App"}
 deployLink = {"https://samrf13.github.io/05-Calendar/"}
 githubRepo  = {"https://github.com/SamRF13/05-Calendar"}
-img = {"/docs/assets/CalendarLogo.png"}
+img = {"/assets/CalendarLogo.png"}
 /> 
 
     </div>
