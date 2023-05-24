c3346942a8bd77baacb749a0715344b4a5f92354
Samuel Russek <samuel_bstone_13@hotmail.com>
2023-05-24 17:49:17 -0600

    pages to components

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,21 +8,21 @@ const Portfolio = () => {
 title = {"Note Taker"}
 deployLink = {"https://calendar-11.herokuapp.com/"}
 githubRepo  = {"https://github.com/SamRF13/11-Note-Creator"}
-img = {"/imagesAssets/NoteTakerImg.png"}
+img = {"/src/pages/images/NoteTakerImg.png"}
 /> 
 
 <Project
 title = {"Weather App"}
 deployLink = {"https://samrf13.github.io/06-Weather-app/"}
 githubRepo  = {"https://github.com/SamRF13/06-Weather-app/"}
-img = {"/imagesAssets/Calendar2Logo.png"}
+img = {"/src/pages/images/Calendar2Logo.png"}
 /> 
 
 <Project
 title = {"Calendar App"}
 deployLink = {"https://samrf13.github.io/05-Calendar/"}
 githubRepo  = {"https://github.com/SamRF13/05-Calendar"}
-img = {"/imagesAssets/CalendarLogo.png"}
+img = {"/src/pages/images/CalendarLogo.png"}
 /> 
 
     </div>
